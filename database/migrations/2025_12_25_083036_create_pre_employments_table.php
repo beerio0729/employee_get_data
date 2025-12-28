18988def50c5b5074caf72510f5d495e9e563b8a
Beerio <beerio0729@gmail.com>
2025-12-29 03:44:43 +0700

    Fix defult applied_at 25-12-25 03:44

--- database/migrations/2025_12_25_083036_create_pre_employments_table.php
+++ database/migrations/2025_12_25_083036_create_pre_employments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignID('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('status_id')->nullable()->constrained('work_status_defination_details')->nullOnDelete();
-            $table->timestamp('applied_at');  // วันที่สมัคร (ต้องมี)
+            $table->timestamp('applied_at')->useCurrent();  // วันที่สมัคร (ต้องมี)
             $table->timestamp('interview_at')->nullable(); // วันสัมภาษณ์ (อาจยังไม่มี)
             $table->timestamp('result_at')->nullable();  // วันที่สรุปผล (อาจยังไม่มี)
             $table->timestamps();
