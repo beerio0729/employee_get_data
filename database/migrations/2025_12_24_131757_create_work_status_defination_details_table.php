adea9f29710c148225a60d54fcc4e61e7d9938ad
Beerio <beerio0729@gmail.com>
2025-12-29 03:30:31 +0700

    Fix color status 25-12-25 02:49

--- database/migrations/2025_12_24_131757_create_work_status_defination_details_table.php
+++ database/migrations/2025_12_24_131757_create_work_status_defination_details_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name_th');
             $table->string('name_en');
             $table->string('work_phase')->nullable(); // ใช้ enum หรือ string ก็ได้
-            $table->enum('color', ['success', 'warning','danger'])->default('warning');
+            $table->enum('color', ['primary', 'success', 'warning', 'danger'])->default('primary');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
