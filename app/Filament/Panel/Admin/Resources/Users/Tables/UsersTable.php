54a3b8f11ebf6fd4398124deba95804882f2e869
Beerio <beerio0729@gmail.com>
2026-01-14 20:23:24 +0700

    แก้ไข้ที่เผลอลบตัว action interview203-01-23 20:23

--- app/Filament/Panel/Admin/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Panel/Admin/Resources/Users/Tables/UsersTable.php
@@ -274,6 +274,76 @@ class UsersTable
 
                         ])
                         ->action(function ($record, array $data) {
+                            $view_notification = 'view_interview_' . Date::now()->timestamp;
+                            $workStatus = $record->userHasoneWorkStatus()->first();
+                            $interview_date = Carbon::parse($data['interview_at'])->locale('th');
+                            if ($data['interview_channel'] === 'online') {
+                                $calendar = new GoogleCalendarService();
+                                $calendar_response = $calendar->createEvent([
+                                    'start_time' => $data['interview_at'],
+                                    'duration' => $data['interview_duration'], //ระยะเวลาการประชุม
+                                    'email' => $record->email,
+                                    'title' => "นัดประชุมคุณ {$record->userHasoneIdcard->name_th} {$record->userHasoneIdcard->last_name_th}",
+                                ]);
+                            }
+
+                            $workStatus->update([
+                                'work_status_def_detail_id' => 3,
+                            ]);
+
+                            $workStatus->workStatusHasonePreEmp()->update([
+                                'google_calendar_id' => $calendar_response?->id ?? null,
+                                'interview_channel' => $data['interview_channel'],
+                                'interview_at' => $data['interview_at'],
+                            ]);
+                            $history = $record->userHasoneHistory();
+                            $history->updateOrCreate(
+                                ['user_id' => $record->id],
+                                [
+                                    'data' => [
+                                        ...$history->first()->data ?? [],
+                                        [
+                                            'event' => 'interview scheduled',
+                                            'description' => "นัดหมายวันนัดสัมภาษณ์ผ่านช่องทาง \"{$data['interview_channel']}\"",
+                                            'value' => $data['interview_at'],
+                                            'date' => carbon::now()->format('y-m-d H:i:s'),
+                                        ]
+                                    ],
+                                ]
+                            );
+                            Notification::make() //ต้องรัน Queue
+                                ->title('แจ้งวันนัดสัมภาษณ์')
+                                ->body("เรียน คุณ {$record->userHasoneIdcard->name_th} {$record->userHasoneIdcard->last_name_th} \n\n"
+                                    . "<br><br>ทางบริษัทฯ ขอแจ้งนัดหมายวันสัมภาษณ์งานของท่านใน<br><B>วัน"
+                                    . $interview_date->translatedFormat('D ที่ j M ')
+                                    . $interview_date->year + 543
+                                    . "\nเวลา "
+                                    . $interview_date->format(' H:i')
+                                    . " น."
+                                    . "</B>"
+                                    . "<br>ผ่านช่องทาง <B>\"" . ucwords($data['interview_channel']) . " \"</B>"
+                                    . "<br><br>โปรดเตรียมเอกสารที่เกี่ยวข้องและมาถึงก่อนเวลานัดหมาย 10 นาที"
+                                    . "<br>ขอบคุณค่ะ")
+                                ->actions([
+                                    Action::make($view_notification)
+                                        ->button()
+                                        ->label('ทำเครื่องหมายว่าอ่านแล้ว')
+                                        ->markAsRead(),
+                                ])
+                                ->sendToDatabase($record, isEventDispatched: true);
+
+                            LineSendMessageService::send($record->provider_id, [
+                                "เรียน คุณ {$record->userHasoneIdcard->name_th} {$record->userHasoneIdcard->last_name_th} \n\n"
+                                    . "ทางบริษัทฯ ขอแจ้งนัดหมายวันสัมภาษณ์งานของท่านใน\n\nวัน "
+                                    . $interview_date->translatedFormat('D ที่ j M ')
+                                    . $interview_date->year + 543
+                                    . "\nเวลา "
+                                    . $interview_date->format(' H:i')
+                                    . " น.\n"
+                                    . "ผ่านช่องทาง \"" . ucwords($data['interview_channel']) . " \"\n\n"
+                                    . "โปรดเตรียมเอกสารที่เกี่ยวข้องและมาถึงก่อนเวลานัดหมาย 10 นาที \n\n"
+                                    . "ขอบคุณค่ะ",
+                            ]);
                             Notification::make()
                                 ->title('นัดหมายวันสัมภาษณ์เรียบร้อยแล้ว')
                                 ->success()
